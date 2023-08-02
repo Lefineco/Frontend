3062aa1e7f8a4990b51533ff0d772994d815c986
Fatih <134298451+fatihhyldz@users.noreply.github.com>
2023-08-02 15:22:53 +0300

    initial

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    "@nuxthq/ui",
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt"
+  ]
+})
