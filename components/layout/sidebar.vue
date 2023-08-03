8314877a93ade6ecdb0d24fa022d8137d185631d
Berke KAYADA <berke.kayada@coino.com>
2023-08-03 17:36:02 +0300

    fix

--- components/layout/sidebar.vue
+++ components/layout/sidebar.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="dark:bg-black !bg-opacity-50 flex h-full  justify-between items-center flex-col">
+  <div class="dark:bg-black  !bg-opacity-50 flex h-full  justify-between items-center flex-col">
     <UButton size="xl" color="white" icon="i-ph-play-circle-fill" variant="link" />
     <UButton size="xl" color="white" icon="i-ph-users-three-light" variant="link" />
     <UButton size="xl" color="white" icon="i-ph-chat-teardrop-dots" variant="link" />
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <LayoutHeader />
   <div class="grid grid-cols-3 gap-4">
-    <LayoutSidebar class="col-span-1 w-[130px] bg-gray-200 p-4" />
+    <LayoutSidebar class="col-span-1 w-[130px]  p-4" />
     <slot class="col-span-2 bg-gray-300 p-4" />
   </div>
 </template>
