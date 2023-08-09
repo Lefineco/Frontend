ae03ee2e863ba82da4e49cca65e874c81a504b15
Furkan Erdogan <furkanerdogan@outlook.com.tr>
2023-08-09 18:05:52 +0300

    login and register components seperated

---
+++ components/Auth/Login/index.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import type { Form } from '@nuxthq/ui/dist/runtime/types'
+import type { Schema } from './schema'
+
+const form = ref<Form<Schema>>()
+const values = ref<Partial<Schema>>({
+  email: undefined,
+  password: undefined,
+})
+</script>
+
+<template>
+  <UForm ref="form" :state="values" class="relative w-full h-full flex items-center justify-center bg-black text-white z-0">
+    <img src="~assets/auth-background.png" class="absolute top-0 right-0 left-0 w-full -z-10">
+    <NuxtLink to="/" class="mx-auto absolute top-20">
+      <img src="~assets/logo.svg">
+    </NuxtLink>
+    <div class="w-1/4 flex flex-col gap-4">
+      <div class="flex gap-2 items-end">
+        <h1 class="text-3xl font-bold capitalize">
+          Login
+        </h1>
+        <span class="text-sm text-gray-400">or</span>
+        <NuxtLink to="register" class="text-[#BFA8FC] underline font-semibold capitalize">
+          Register
+        </NuxtLink>
+      </div>
+
+      <p class="text-sm text-gray-400">
+        How do I get started blazein dolor at?
+      </p>
+
+      <UButton icon="i-devicon-google" size="xl" variant="solid" label="Sign in with Google" block class="text-sm font-light !bg-[#FFFFFF33] hover:!bg-[#FFFFFF50] transition" />
+
+      <p class="text-sm text-gray-400">
+        or
+      </p>
+
+      <UFormGroup name="email">
+        <UInput v-model="values.email" icon="i-ph-user-fill" size="xl" variant="none" placeholder="Username or Email" class="!bg-[#ffffff1a] !text-xs py-4" />
+      </UFormGroup>
+      <UFormGroup name="password">
+        <UInputPassword v-model="values.password" size="xl" variant="none" placeholder="Password" class="!bg-[#ffffff1a] !text-xs py-4" />
+      </UFormGroup>
+
+      <div class="flex items-center justify-between text-sm">
+        <UCheckbox label="Remember me" />
+        <NuxtLink to="/auth/forgot" class="text-[#BFA8FC]">
+          Forgot Password?
+        </NuxtLink>
+      </div>
+
+      <UButton size="xl" variant="solid" label="Sign In" block class="font-light" />
+    </div>
+  </UForm>
+</template>
---
+++ composables/auth.ts
@@ -0,0 +1,61 @@
+import type { AuthError } from '@supabase/supabase-js'
+
+interface ILoginOrRegisterPayload {
+  email: string
+  password: string
+}
+
+function useAuth() {
+  const supabase = useSupabaseClient()
+  const toast = useToast()
+  const router = useRouter()
+
+  const errorToast = (error: AuthError | null) => {
+    return toast.add({
+      id: 'error',
+      title: error?.name,
+      description: error?.message,
+      icon: 'i-octicon-desktop-download-24',
+      timeout: 2000,
+      color: 'red',
+    })
+  }
+
+  const successToast = (title: string) => toast.add({
+    id: 'success',
+    title,
+    icon: 'i-octicon-desktop-download-24',
+    timeout: 2000,
+    color: 'green',
+  })
+
+  const signIn = async (payload: ILoginOrRegisterPayload) => {
+    const { error } = await supabase.auth.signInWithPassword(payload)
+
+    if (error) {
+      errorToast(error)
+    }
+
+    else {
+      successToast('Signed in successfully!')
+      router.push('/')
+    }
+  }
+
+  const signUp = async (payload: ILoginOrRegisterPayload) => {
+    const { error } = await supabase.auth.signUp(payload)
+
+    if (error) {
+      errorToast(error)
+    }
+
+    else {
+      successToast('Signed up successfully!')
+      router.push('/')
+    }
+  }
+
+  return { signIn, signUp }
+}
+
+export default useAuth
