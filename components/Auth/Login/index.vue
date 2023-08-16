e57b6b5ff923459ad48b8fb90bed48bde793e9ea
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-08-17 00:27:44 +0300

    google auth & signout

--- components/Auth/Login/index.vue
+++ components/Auth/Login/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { Form } from '@nuxthq/ui/dist/runtime/types'
+import type { Form } from 'node_modules/@nuxthq/ui/dist/runtime/types'
 import { loginSchema } from './schema'
 import type { LoginSchema } from './schema'
 import useAuth from '@/composables/auth'
@@ -7,6 +7,7 @@ import useAsync from '@/composables/async'
 
 const { signIn } = useAuth()
 const { loading, makeAsyncOperation } = useAsync()
+const supabase = useSupabaseClient()
 
 const form = ref<Form<LoginSchema>>()
 const values = ref<Partial<LoginSchema>>({
@@ -20,10 +21,22 @@ async function onSubmit() {
     await signIn(values.value as LoginSchema)
   })
 }
+
+function signInWithGoogle() {
+  supabase.auth.signInWithOAuth({
+    provider: 'google',
+  })
+}
 </script>
 
 <template>
-  <UForm ref="form" :schema="loginSchema" :state="values" class="relative w-full h-full flex flex-col items-center justify-center text-white z-0" @submit.prevent="onSubmit">
+  <UForm
+    ref="form"
+    :schema="loginSchema"
+    :state="values"
+    class="relative w-full h-full flex flex-col items-center justify-center text-white z-0"
+    @submit.prevent="onSubmit"
+  >
     <NuxtLink to="/" class="mx-auto absolute top-20">
       <img src="~assets/logo.svg">
     </NuxtLink>
@@ -33,7 +46,10 @@ async function onSubmit() {
           Login
         </h1>
         <span class="text-sm text-gray-400">or</span>
-        <NuxtLink to="register" class="text-[#BFA8FC] underline font-semibold capitalize">
+        <NuxtLink
+          to="register"
+          class="text-[#BFA8FC] underline font-semibold capitalize"
+        >
           Register
         </NuxtLink>
       </div>
@@ -42,14 +58,24 @@ async function onSubmit() {
         How do I get started blazein dolor at?
       </p>
 
-      <UButton icon="i-devicon-google" label="Sign in with Google" variant="secondary" block />
+      <UButton
+        icon="i-devicon-google"
+        label="Sign in with Google"
+        variant="secondary"
+        block
+        @click="signInWithGoogle()"
+      />
 
       <p class="text-sm text-gray-400">
         or
       </p>
 
       <UFormGroup name="email" size="xs">
-        <UInput v-model="values.email" icon="i-ph-user-fill" placeholder="Username or Email" />
+        <UInput
+          v-model="values.email"
+          icon="i-ph-user-fill"
+          placeholder="Username or Email"
+        />
       </UFormGroup>
       <UFormGroup name="password" size="xs">
         <UInputPassword v-model="values.password" placeholder="Password" />
--- components/layout/header.vue
+++ components/layout/header.vue
@@ -3,27 +3,40 @@ import { ref } from 'vue'
 import logo from '@/assets/logo.svg'
 
 const user = useSupabaseUser()
+const supabase = useSupabaseClient()
 
 const items = [
-  [{
-    label: user.value && (user.value.name || user.value.email),
-    slot: 'account',
-    disabled: true,
-  }], [{
-    label: 'Settings',
-    to: '/profile',
-    icon: 'i-ph-gear-six',
-  }], [
+  [
+    {
+      label: user.value && (user.value.name || user.value.email),
+      slot: 'account',
+      disabled: true,
+    },
+  ],
+  [
+    {
+      label: 'Settings',
+      to: '/profile',
+      icon: 'i-ph-gear-six',
+    },
+  ],
+  [
     {
       label: 'Changelog',
       icon: 'i-heroicons-megaphone',
-    }, {
+    },
+    {
       label: 'Status',
       icon: 'i-heroicons-signal',
-    }], [{
-    label: 'Sign out',
-    icon: 'i-ph-sign-out',
-  }],
+    },
+  ],
+  [
+    {
+      label: 'Sign out',
+      icon: 'i-ph-sign-out',
+      onclick: () => supabase.auth.signOut(),
+    },
+  ],
 ]
 
 const isClicked = ref(false)
@@ -51,9 +64,21 @@ function handleInputBlur() {
     </div>
 
   </div> -->
-  <div class="bg-black/5 border-b border-white/5 shadow-sm backdrop-blur-md fixed inset-x-0 top-0 z-10 flex justify-between items-center md:px-6 py-3 max-md: px-3">
-    <div v-if="isClicked" class="w-full ">
-      <UInput ref="inputRef" v-model="q" name="q" autofocus size="sm" placeholder="Search Video, Lefiner or Room" icon="i-heroicons-magnifying-glass-20-solid" :ui="{ icon: { trailing: { pointer: '' } } }" @blur="handleInputBlur">
+  <div
+    class="bg-black/5 border-b border-white/5 shadow-sm backdrop-blur-md fixed inset-x-0 top-0 z-10 flex justify-between items-center md:px-6 py-3 max-md: px-3"
+  >
+    <div v-if="isClicked" class="w-full">
+      <UInput
+        ref="inputRef"
+        v-model="q"
+        name="q"
+        autofocus
+        size="sm"
+        placeholder="Search Video, Lefiner or Room"
+        icon="i-heroicons-magnifying-glass-20-solid"
+        :ui="{ icon: { trailing: { pointer: '' } } }"
+        @blur="handleInputBlur"
+      >
         <template #trailing>
           <UButton
             color="gray"
@@ -71,7 +96,16 @@ function handleInputBlur() {
         <img :src="logo" class="h-9">
       </div>
       <div class="w-9/12 lg:w-[500px] max-md:hidden max-lg:px-8">
-        <UInput v-model="q" name="q" autofocus size="sm" placeholder="Search Video, Lefiner or Room" :ui="{ icon: { trailing: { pointer: '' } } }" icon="i-heroicons-magnifying-glass-20-solid" @blur="handleInputBlur">
+        <UInput
+          v-model="q"
+          name="q"
+          autofocus
+          size="sm"
+          placeholder="Search Video, Lefiner or Room"
+          :ui="{ icon: { trailing: { pointer: '' } } }"
+          icon="i-heroicons-magnifying-glass-20-solid"
+          @blur="handleInputBlur"
+        >
           <template #trailing>
             <UButton
               color="gray"
@@ -83,10 +117,22 @@ function handleInputBlur() {
           </template>
         </UInput>
       </div>
-      <div class=" space-x-2  flex items-center justify-center" :class="{ hidden: isClicked }">
-        <UIcon class="md:hidden w-6 h-6" :class="{ hidden: isClicked }" name="i-heroicons-magnifying-glass-20-solid" @click="handleClick" />
+      <div
+        class="space-x-2 flex items-center justify-center"
+        :class="{ hidden: isClicked }"
+      >
+        <UIcon
+          class="md:hidden w-6 h-6"
+          :class="{ hidden: isClicked }"
+          name="i-heroicons-magnifying-glass-20-solid"
+          @click="handleClick"
+        />
         <div v-if="user" class="flex gap-2">
-          <UDropdown :items="items" :ui="{ item: { disabled: 'cursor-text select-text' } }" :popper="{ placement: 'bottom-end' }">
+          <UDropdown
+            :items="items"
+            :ui="{ item: { disabled: 'cursor-text select-text' } }"
+            :popper="{ placement: 'bottom-end' }"
+          >
             <UAvatar variant="ghost" :src="user.picture" :alt="user.email" />
 
             <template #account="{ item }">
@@ -94,7 +140,9 @@ function handleInputBlur() {
                 <p class="text-xs">
                   Signed in as
                 </p>
-                <p class="truncate w-36 font-medium text-gray-900 dark:text-white">
+                <p
+                  class="truncate w-36 font-medium text-gray-900 dark:text-white"
+                >
                   {{ item.label }}
                 </p>
               </div>
