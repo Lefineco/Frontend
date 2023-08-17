8d2b47cf3b4028e7d10f670b50079367afe2740b
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-08-17 22:28:01 +0300

    ...

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -55,15 +55,6 @@ function handleInputBlur() {
 </script>
 
 <template>
-  <!-- <div class="bg-black/5 border-b border-white/5 shadow-sm backdrop-blur-md fixed inset-x-0 top-0 z-10 flex justify-between items-center px-6 py-3 h-16">
-    <div class="flex items-center gap-3">
-      <img :src="logo" class="h-9">
-    </div>
-    <div class="w-[500px]">
-      <UInput size="sm" placeholder="Search Video, Lefiner or Room" icon="i-heroicons-magnifying-glass-20-solid" />
-    </div>
-
-  </div> -->
   <div
     class="bg-black/5 border-b border-white/5 shadow-sm backdrop-blur-md fixed inset-x-0 top-0 z-10 flex justify-between items-center md:px-6 py-3 max-md: px-3"
   >
