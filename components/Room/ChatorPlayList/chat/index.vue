2f0177b3705c744762d09fe4dbc9317e63fbdf44
Berke KAYADA <berke.kayada@primetech.com>
2023-08-28 17:51:12 +0300

    scroll bottom chat

--- components/Room/ChatorPlayList/chat/index.vue
+++ components/Room/ChatorPlayList/chat/index.vue
@@ -1,17 +1,40 @@
 <script setup lang="ts">
 const messageData = reactive([
-
+  {
+    message: 'heleley',
+    uuid: 'berke',
+  },
+  {
+    message: 'heleley',
+    uuid: 'berke',
+  },
+  {
+    message: 'heleley',
+    uuid: 'asd',
+  },
 ])
+const chatContainerRef = ref<HTMLElement | null>(null)
+
+onMounted(() => {
+  if (chatContainerRef.value)
+    chatContainerRef.value.scrollTop = chatContainerRef.value.scrollHeight
+})
+
+function scrollToBottom() {
+  if (chatContainerRef.value)
+    chatContainerRef.value.scrollTop = chatContainerRef.value.scrollHeight
+}
 
 function veriyiAl(veri: { message: string; uuid: string }) {
   messageData.push({ ...veri })
+  scrollToBottom()
 }
 </script>
 
 <template>
   <div class="flex flex-col justify-between max-h-[600px] h-full  ">
-    <div class="relative flex flex-col gap-7 overflow-scroll">
-      <RoomChatorPlayListChatMessage v-for="(item, idx) in messageData" :key="idx" :message="item.message" :uuid="item.uuid" />
+    <div ref="chatContainerRef" class="relative flex flex-col gap-7 overflow-scroll">
+      <RoomChatorPlayListChatMessage v-for="(item, idx) in messageData" :key="idx" class=".chat-container" :message="item.message" :uuid="item.uuid" />
     </div>
     <RoomChatorPlayListChatInput @message="veriyiAl" />
   </div>
