c0ab5fab990bb7629d6c965de352157b22664910
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-09-05 00:38:50 +0300

    deployment fix

--- components/Cards/profile.vue
+++ components/Cards/profile.vue
@@ -18,7 +18,7 @@ withDefaults(defineProps<Props>(), {
 <template>
   <div class=" relative w-full h-[230px] lg:w-[400px]  lg:h-[160px]  flex flex-col items-center justify-center  rounded-[20px]">
     <div class="relative overflow-hidden rounded-2xl w-full after:content-[''] after:absolute after:inset-0 after:bg-black/60 ">
-      <img class="  h-full w-full object-cover rounded-[20px]" src="../../assets/testimage.svg">
+      <img class="  h-full w-full object-cover rounded-[20px]" src="https://images.unsplash.com/photo-1693697117720-c5e098ecf350?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80">
     </div>
     <div class="absolute max-lg:items-center w-full flex-col flex  lg:flex-row justify-between bottom-1 py-2 px-5">
       <div class="flex flex-col lg:flex-row items-center justify-center gap-3">
