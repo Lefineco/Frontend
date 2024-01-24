e2880112de956bb06f6698f5603cfe4f8fd1dd3d
Resat Akkus <resatakkus10@gmail.com>
2024-01-24 21:33:53 +0300

    style refactored community post create

--- components/Feeds/Form/Create/index.vue
+++ components/Feeds/Form/Create/index.vue
@@ -3,35 +3,36 @@ const value = ref('')
 </script>
 
 <template>
-	<div class="w-full flex flex-col rounded-3xl bg-white/5">
+	<div class="container">
 		<UTextarea
-			v-model="value"
-			variant="none"
-			:ui="{
+			v-model="value" variant="none" :ui="{
 				variant: {
 					none: 'text-sm font-medium focus:ring-2 focus:ring-violet-500 p-4 rounded-t-3xl rounded-b-none',
 				},
-			}"
-			placeholder="Write something or ask..."
+			}" placeholder="Write something or ask..."
 		/>
-		<hr class="border-white/5">
+		<hr class="divider">
 
-		<div class="flex items-center justify-between p-4">
+		<div class="footer">
 			<div>
-				<UButton
-					variant="ghost"
-					color="white"
-					icon="i-ph-image-square-fill"
-					label="Add Photo"
-				/>
-				<UButton
-					variant="ghost"
-					color="white"
-					icon="i-ph-calendar"
-					label="Create Activity"
-				/>
+				<UButton variant="ghost" color="white" icon="i-ph-image-square-fill" label="Add Photo" />
+				<UButton variant="ghost" color="white" icon="i-ph-calendar" label="Create Activity" />
 			</div>
 			<UButton>Create Feed</UButton>
 		</div>
 	</div>
 </template>
+
+<style lang="postcss" scoped>
+.container {
+	@apply w-full flex flex-col rounded-3xl bg-white/5
+}
+
+.divider {
+	@apply border-white/5
+}
+
+.footer {
+	@apply flex items-center justify-between p-4
+}
+</style>
