b77809c13a6a6b7b3a0f5161a26888fd6610a83a
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-02-25 12:09:03 +0300

    feat: tmbd utils

---
+++ server/utils/tmbd.ts
@@ -0,0 +1,14 @@
+import type { $Fetch } from 'ofetch'
+import { ofetch } from 'ofetch'
+
+export function tmbd(): { fetch: $Fetch } {
+	const fetch = ofetch.create({
+		baseURL: 'https://api.themoviedb.org/3',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${useRuntimeConfig(useEvent()).tmbdToken}`,
+		},
+	})
+
+	return { fetch }
+}
